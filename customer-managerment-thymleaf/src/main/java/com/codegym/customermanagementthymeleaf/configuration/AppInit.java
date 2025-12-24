bd9850c461f9be98cd4cc144ffdafcfbfd35f550
Yuhhna2907 <luonganhhuy2004@gmail.com>
2025-12-24 13:54:04 +0700

    Add files via upload

---
+++ customer-managerment-thymleaf/src/main/java/com/codegym/customermanagementthymeleaf/configuration/AppInit.java
@@ -0,0 +1,21 @@
+package com.codegym.customermanagementthymeleaf.configuration;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
