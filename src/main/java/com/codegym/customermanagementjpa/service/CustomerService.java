865ee47e20944a2c752d613bd2073415ffe6dcd5
Yuhhna2907 <luonganhhuy2004@gmail.com>
2025-12-29 15:47:30 +0700

    Add files via upload

---
+++ src/main/java/com/codegym/customermanagementjpa/service/CustomerService.java
@@ -0,0 +1,36 @@
+package com.codegym.customermanagementjpa.service;
+
+import com.codegym.customermanagementjpa.model.Customer;
+import com.codegym.customermanagementjpa.repository.ICustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService implements ICustomerService {
+    private final ICustomerRepository iCustomerRepository;
+
+    public CustomerService(ICustomerRepository iCustomerRepository) {
+        this.iCustomerRepository = iCustomerRepository;
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return iCustomerRepository.findAll();
+    }
+
+    @Override
+    public void save(Customer customer) {
+        iCustomerRepository.save(customer);
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return iCustomerRepository.findById(id);
+    }
+
+    @Override
+    public void remove(Long id) {
+        iCustomerRepository.remove(id);
+    }
+}
